952e606fd0185f1d6ef3940e75f5d0d178c395ce
Junior Sanchez <junior5783@gmail.com>
2022-06-22 17:41:01 -0500

    Adding admin restrictions

--- application.js
+++ application.js
@@ -4,6 +4,17 @@ const shoppingCarRouter = require('./routers/shopping-car-router');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+const isAdmin = true;
+const isAdminUserMiddleware = (req,res,next) => {
+    console.log(`Logged  ${req.url}  ${req.method} -- ${new Date()}`)
+
+    if(!isAdmin && req?.url?.startsWith('/api/productos') && ['POST', 'PUT', 'DELETE'].includes(req.method)){
+        return res.status(401).json({message: "Esta API solo esta permitida para administradores"});
+    }
+    next();
+};
+
+app.use(isAdminUserMiddleware);
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use('/api/productos', productsRouter);
